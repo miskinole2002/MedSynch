deb7d3da5e41bd482aa1a85915fa0a0d49045507
miskinole2002 <sadeufotsing2002@gmail.com>
2025-03-04 12:39:11 -0500

    +

--- src/Components/Register/Register_medecin.jsx
+++ src/Components/Register/Register_medecin.jsx
@@ -3,7 +3,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Api from "../../contexts/request";
 
-import styles from "./register.module.scss"
+import styles from "./Register.module.scss"
 import { useContext } from "react";
 
 
--- src/Components/Register/Register_patient.jsx
+++ src/Components/Register/Register_patient.jsx
@@ -3,7 +3,7 @@ import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import Api from "../../contexts/request";
 import { useContext } from "react";
-import styles from "./register.module.scss"
+import styles from "./Register.module.scss"
 
 
 
